95cf7a951f610b9d87da7a089c8b9311b18346c6
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-08-21 01:15:06 +0900

    add swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,25 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+// console.log("123")
+// alert("1111
+
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  // 分頁   
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  // 左右箭頭    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 滾動條
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
 });
