aa139aee47f38a35e3fc13e2b401d9ee67fd1678
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-08-30 21:22:21 +0900

    Update 2022-08-30T12:22:13.379Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,50 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+// console.log("123")
+// alert("1111
+// import Swiper from 'swiper';
+AOS.init();
+var swiper = new Swiper(".swiper", {
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 0
+  },
+  speed: 3000,
+  loop: true,
+  watchSlidesProgress: true,
+  // spaceBetween: 16,
+  // slidesPerView: 10,
+  //多欄
+  breakpoints: {
+    480: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 4
+    },
+    1200: {
+      slidesPerView: 5
+    }
+  }
+}); // Swiper
+
+var swiperComment = new Swiper('.swiper-comment', {
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
 });
 //# sourceMappingURL=all.js.map
